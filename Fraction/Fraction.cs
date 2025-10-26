4695ffa6a954614e38a4e2ef31426aa6a159cad0
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-10-26 19:17:47 +0300

    Fraction in progress

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+	internal class Fraction
+	{
+		private int denominator;
+		public int Integer { get; set; }
+		public int Numerator { get; set; }
+		public int Denominator
+		{
+			get { return denominator; }
+			set { denominator = (value == 0 ? 1 : value); }
+		}
+		public Fraction()
+		{
+			Integer = 0;
+			Numerator = 0;
+			Denominator = 1;
+			Console.WriteLine($"DefaultConstructor: {this.GetHashCode()}");
+		}
+		public Fraction(double value)
+		{
+			const int precision = 1000000000;
+			Integer = (int)value;
+			Numerator = (int)Math.Round((value - Integer) * precision);
+			Denominator = precision;
+			ReduceFraction(this);
+			Console.WriteLine($"SingleArgumentConstructor: {this.GetHashCode()}");
+		}
+		public Fraction(int numerator, int denominator)
+		{
+			Integer = 0;
+			Numerator = numerator;
+			Denominator = denominator;
+			Console.WriteLine($"Constructor: {this.GetHashCode()}");
+		}
+		public Fraction(int integer, int numerator, int denominator)
+		{
+			Integer = integer;
+			Numerator = numerator;
+			Denominator = denominator;
+			Console.WriteLine($"Constructor: {this.GetHashCode()}");
+		}
+		public Fraction(Fraction other)
+		{
+			Integer = other.Integer;
+			Numerator = other.Numerator;
+			Denominator = other.Denominator;
+			Console.WriteLine($"CopyConstructor: {this.GetHashCode()}");
+		}
+		~Fraction()
+		{
+			Console.WriteLine($"Destructor: {this.GetHashCode()}");
+		}
+		//public Fraction operator=(Fraction other)
+		//{
+		//	Console.WriteLine($"CopyAssignment: {this.GetHashCode()}");
+		//	return new Fraction(other);
+		//}
+		//public Fraction operator+=(Fraction other)
+		//{
+		//	if (this.Denominator == other.Denominator)
+		//	{
+		//		this.Integer += other.Integer;
+		//		this.Numerator += other.Numerator;
+		//	}
+		//	else
+		//	{
+		//		int lcm = LCM(this.Denominator, other.Denominator);
+		//		this.Integer += other.Integer;
+		//		this.Numerator = this.Numerator * (lcm / this.Denominator) + other.Numerator * (lcm / other.Denominator);
+		//		this.Denominator = lcm;
+		//	}
+		//	this = ToProper(this);
+		//	return This;
+		//}
+		//public Fraction operator-=(Fraction other)
+		//{
+		//	if (this.Denominator == other.Denominator)
+		//	{
+		//		this.Integer -= other.Integer;
+		//		this.Numerator -= other.Numerator;
+		//	}
+		//	else
+		//	{
+		//		int lcm = LCM(this.Denominator, other.Denominator);
+		//		this.Integer -= other.Integer;
+		//		this.Numerator = this.Numerator * (lcm / this.Denominator) - other.Numerator * (lcm / other.Denominator);
+		//		this.Denominator = lcm;
+		//	}
+		//	this = ToProper(this);
+		//	return this;
+		//}
+		//public Fraction operator*=(Fraction other)
+		//{
+		//	return new Fraction(this * other);
+		//}
+		//public Fraction operator/=(Fraction other)
+		//{
+		//	return new Fraction(this / other);
+		//}
+		public static Fraction operator++(Fraction This)
+		{
+			return new Fraction(This.Integer + 1, This.Numerator, This.Denominator);
+		}
+		//public static Fraction operator++(Fraction This, int postfix)
+		//{
+		//	return new Fraction(This.Integer, This.Numerator, This.Denominator);
+		//}
+		public static Fraction operator--(Fraction This)
+		{
+			return new Fraction(This.Integer - 1, This.Numerator, This.Denominator);
+		}
+		//public static Fraction operator--(Fraction This, int postfix)
+		//{
+		//	return new Fraction(This.Integer, This.Numerator, This.Denominator);
+		//}
+		public static Fraction operator*(Fraction left, Fraction right)
+		{
+			left = ToImproper(left);
+			right = ToImproper(right);
+			return ToProper(ReduceFraction(new Fraction(left.Numerator * right.Numerator, left.Denominator * right.Denominator)));
+		}
+		public static Fraction operator/(Fraction left, Fraction right)
+		{
+			return new Fraction(left * Invert(right));
+		}
+		public static Fraction operator+(Fraction left, Fraction right)
+		{
+			if (left.Denominator == right.Denominator) return new Fraction
+			(
+				left.Integer + right.Integer,
+				left.Numerator + right.Numerator,
+				left.Denominator
+			);
+			int lcm = LCM(left.Denominator, right.Denominator);
+			return ToProper(new Fraction
+			(
+				left.Integer + right.Integer,
+				left.Numerator * (lcm / left.Denominator) +
+				right.Numerator * (lcm / right.Denominator),
+				lcm
+			));
+		}
+		public static Fraction operator-(Fraction left, Fraction right)
+		{
+			if (left.Denominator == right.Denominator) return new Fraction
+			(
+				left.Integer - right.Integer,
+				left.Numerator - right.Numerator,
+				left.Denominator
+			);
+			int lcm = LCM(left.Denominator, right.Denominator);
+			return ToProper(new Fraction
+			(
+				left.Integer - right.Integer,
+				left.Numerator * (lcm / left.Denominator) -
+				right.Numerator * (lcm / right.Denominator),
+				lcm
+			));
+		}
+		public static bool operator==(Fraction left, Fraction right)
+		{
+			left = ToImproper(left);
+			right = ToImproper(right);
+			return left.Numerator * right.Denominator == left.Denominator * right.Numerator;
+		}
+		public static bool operator!=(Fraction left, Fraction right)
+		{
+			return !(left == right);
+		}
+		public static bool operator>(Fraction left, Fraction right)
+		{
+			left = ToImproper(left);
+			right = ToImproper(right);
+			return left.Numerator * right.Denominator > left.Denominator * right.Numerator;
+		}
+		public static bool operator<(Fraction left, Fraction right)
+		{
+			left = ToImproper(left);
+			right = ToImproper(right);
+			return left.Numerator * right.Denominator < left.Denominator * right.Numerator;
+		}
+		public static bool operator>=(Fraction left, Fraction right)
+		{
+			return !(left < right);
+		}
+		public static bool operator<=(Fraction left, Fraction right)
+		{
+			return !(left > right);
+		}
+		public static explicit operator int(Fraction This)
+		{
+			return This.Integer + This.Numerator / This.Denominator;
+		}
+		public static explicit operator double(Fraction This)
+		{
+			return This.Integer + (double)This.Numerator / This.Denominator;
+		}
+		public void Print()
+		{
+			if (Integer != 0) Console.Write(Integer);
+			if (Numerator != 0)
+			{
+				if (Integer != 0) Console.Write("(");
+				Console.Write($"{Numerator} / {Denominator}");
+				if (Integer != 0) Console.Write(")");
+			}
+			else if (Integer == 0) Console.Write(0);
+			Console.WriteLine();
+		}
+		public static int GCD(int a, int b)
+		{
+			if (a == 0) return b;
+			return GCD(b % a, a);
+		}
+		public static int LCM(int a, int b)
+		{
+			return (a * b) / GCD(a, b);
+		}
+		public static Fraction ReduceFraction(Fraction other)
+		{
+			int gcd = GCD(other.Numerator, other.Denominator);
+			other.Numerator /= gcd;
+			other.Denominator /= gcd;
+			return other;
+		}
+		public Fraction ReduceFraction()
+		{
+			int gcd = GCD(this.Numerator, this.Denominator);
+			this.Numerator /= gcd;
+			this.Denominator /= gcd;
+			return this;
+		}
+		public static Fraction ToProper(Fraction other)
+		{
+			other.Integer += other.Numerator / other.Denominator;
+			other.Numerator %= other.Denominator;
+			return other;
+		}
+		public static Fraction ToImproper(Fraction other)
+		{
+			other.Numerator += other.Integer * other.Denominator;
+			other.Integer = 0;
+			return other;
+		}
+		public Fraction ToImproper()
+		{
+			this.Numerator += this.Integer * this.Denominator;
+			this.Integer = 0;
+			return this;
+		}
+		public static Fraction Invert(Fraction other)
+		{
+			other = ToImproper(other);
+			int temp = other.Numerator;
+			other.Numerator = other.Denominator;
+			other.Denominator = temp;
+			return other;
+		}
+	}
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,121 @@
+﻿//#define CONSTRUCTORS
+//#define ARITHMETICAL_OPERATORS
+//#define INCREMENT_DECREMENT
+//#define COMPARISON_OPERATORS
+//#define CONVERSION_FROM_CLASS_TO_OTHER
+#define HAVE_A_NICE_DAY
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+#if CONSTRUCTORS
+			Fraction A = new Fraction();
+			A.Print();
+
+			Fraction B = new Fraction(5);
+			B.Print();
+
+			Fraction C = new Fraction(1, 2);
+			C.Print();
+
+			Fraction D = new Fraction(2, 3, 4);
+			D.Print();
+
+			Fraction E = new Fraction(D);
+			E.Print();
+#endif
+
+#if ARITHMETICAL_OPERATORS
+			Fraction A = new Fraction(2, 3, 4);
+			A.Print();
+			Fraction B = new Fraction(3, 4, 5);
+			B.Print();
+
+			Fraction C = new Fraction(A * B);
+			Console.Write("Проивзедение: "); C.Print();
+
+			C = A / B;
+			Console.Write("Частное: "); C.Print();
+
+			C = A + B;
+			Console.Write("Сумма: "); C.Print();
+
+			C = A - B;
+			Console.Write("Разность: "); C.Print();
+
+			A += B;
+			Console.Write("Сумма (+=): "); A.Print();
+
+			Fraction D = new Fraction(1, 2, 3);
+			Fraction E = new Fraction(2, 3, 4);
+
+			D -= E;
+			Console.Write("Разность (-=): "); D.Print();
+
+			Fraction F = new Fraction(1, 2, 3);
+			Fraction G = new Fraction(2, 3, 4);
+
+			F *= G;
+			Console.Write("Произведение (*=): "); F.Print();
+
+			F /= G;
+			Console.Write("Частное (/=): "); F.Print();
+#endif
+
+#if INCREMENT_DECREMENT
+			Fraction H = new Fraction(1, 2, 3);
+			Fraction I = new Fraction(2, 3, 4);
+
+			I = ++H;
+			Console.Write("Префиксный инкремент: "); I.Print();
+
+			I = H++;
+			Console.Write("Постфиксный инкремент: "); I.Print();
+#endif
+
+#if COMPARISON_OPERATORS
+			Fraction J = new Fraction(1, 5, 3); J.Print();
+			Fraction K = new Fraction(1, 2, 3); K.Print();
+
+			Console.WriteLine($"Оператор сравнения (==): {J == K}");
+			Console.WriteLine($"Оператор сравнения (!=): {J != K}");
+			Console.WriteLine($"Оператор сравнения (>): {J > K}");
+			Console.WriteLine($"Оператор сравнения (<): {J < K}");
+			Console.WriteLine($"Оператор сравнения (>=): {J >= K}");
+			Console.WriteLine($"Оператор сравнения (<=): {J <= K}");
+
+			Fraction L = new Fraction(1, 15, 3);
+
+			L.ReduceFraction();
+			Console.Write($"Сокращение дроби: "); L.Print();
+#endif
+
+#if CONVERSION_FROM_CLASS_TO_OTHER
+			Fraction A = new Fraction(1, 2, 3); A.Print();
+			A.ToImproper();
+
+			int a = (int)A;
+
+			Console.WriteLine(a);
+
+			double b = (double)A;
+
+			Console.WriteLine(b);
+#endif
+
+#if HAVE_A_NICE_DAY
+			Fraction A = new Fraction(2.75);
+			A.Print();
+#endif
+		}
+	}
+}
